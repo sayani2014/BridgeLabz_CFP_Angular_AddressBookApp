81babd17ad0cf4d7bea5821524faccf776a7c220
Sayani Koley <sayanikoley8@gmail.com>
2021-09-13 12:24:41 +0530

    Ability to add form validation and disable input fields of the Edit page

--- address-book/src/app/component/add/add.component.ts
+++ address-book/src/app/component/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { Addressbook } from 'src/app/model/addressbook';
 import { HttpService } from 'src/app/service/http.service';
 import { SnackBarComponent } from '../snack-bar/snack-bar.component';
@@ -67,29 +68,31 @@ export class AddComponent implements OnInit {
    * Purpose: This method is called when a person details need to be updated.
    *          this.route.snapshot.params['id'] check the person id send in the url of the edit page.
    *          this.httpService.getDataByID(), when subscribed, fills the edit form with the person details.
+   *          disable() is used to disable the input field so that the user is not able to edit the fields.
    */
 
 
   editform(): void {
     if (this.route.snapshot.params['id'] != undefined) {
-      this.id = this.route.snapshot.params['id']
+      this.addressFormGroup.controls['name'].disable();
+      this.addressFormGroup.controls['phoneNo'].disable();
+      this.id = this.route.snapshot.params['id'];
       this.httpService.getDataByID(this.id).subscribe(x => {
         this.addressFormGroup.patchValue(x.data);
         if(x.data.state) {
-          this.getCity(x.data.state);
+          this.cityList(x.data.state);
         }
       });
     }  
   }
 
   /**
-   * Purpose: getCity() is called whenever the value is selected from the State dropdown.
-   *          Each and every state has its own cities customized in it.
+   * Purpose: cityList() populates the city based on the stateName selected by the user.
    * 
    * @param state name of the state which is selected by the user from the dropdown. 
    */
 
-  getCity(state) { 
+  cityList(state) {
     for(let i = 0; i < this.stateDetails.length; i++) {
       if(this.stateDetails[i]?.stateName === state) {
         this.cities = this.stateDetails[i]?.city;
@@ -98,7 +101,22 @@ export class AddComponent implements OnInit {
 	}
 
   /**
+   * Purpose: getCity() is called whenever the value is selected from the State dropdown.
+   *          Each and every state has its own cities customized in it.
+   *          cityList() populates the city based on the stateName selected by the user.
+   * 
+   * @param state name of the state which is selected by the user from the dropdown.
+   */
+
+  getCity(state) {
+    this.addressFormGroup.get('city').setValue(""); 
+    this.cityList(state);
+	}
+
+  /**
    * Purpose: onSubmit() is called for submitting a form during Add or Edit form.
+   *          enable() is used to enable the disable input fields of the Edit form.
+   *          The invalid property checks whether the form fields value are valid or not.
    *          The url is checked initially. If it contains any id value of the person details, then the UPDATE HTTP Method is called.
    *          If the url does not have any id, the the CREATE HTTP Method is called.
    *          Finally a success message is displayed to the user using this._snackBar.openSnackBar().
@@ -106,25 +124,31 @@ export class AddComponent implements OnInit {
    */
   
   onSubmit() {
+    this.addressFormGroup.controls['name'].enable();
+    this.addressFormGroup.controls['phoneNo'].enable();
     this.addressbook = this.addressFormGroup.value;
-    if(this.route.snapshot.params['id'] != undefined) {
-      this.httpService.updateData(this.route.snapshot.params['id'], this.addressbook).subscribe(data=>{
-        console.log(data);
-        this._snackBar.openSnackBar(data.message,'X','green-snackbar');
-        this.router.navigateByUrl("/home"); 
-      });
-    }
-    else { 
-      this.httpService.postData(this.addressbook).subscribe(res=>{
-        console.log(res); 
-          this._snackBar.openSnackBar(res.message,'X','green-snackbar');
-          this.router.navigateByUrl("/home");   
-      },error => {
-        this._snackBar.openSnackBar("Person Details already exist!",'X','red-snackbar');
-      });
+    if(this.addressFormGroup.invalid) {
+      this._snackBar.openSnackBar("Please complete the person details!",'X','red-snackbar');
+    } else {
+      if(this.route.snapshot.params['id'] != undefined) {
+        this.httpService.updateData(this.route.snapshot.params['id'], this.addressbook).subscribe(data=>{
+          console.log(data);
+          this._snackBar.openSnackBar(data.message,'X','green-snackbar');
+          this.router.navigateByUrl("/home"); 
+        });
+      }
+      else { 
+        this.httpService.postData(this.addressbook).subscribe(res=>{
+          console.log(res); 
+            this._snackBar.openSnackBar(res.message,'X','green-snackbar');
+            this.router.navigateByUrl("/home");   
+        },error => {
+          this._snackBar.openSnackBar("Person Details already exist!",'X','red-snackbar');
+        });
+      }
     }
   }
-
+  
   /**
    * Purpose: checkError() is called during validation of the form fields.
    * 
